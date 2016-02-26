76d27ef626c5e74dfc7e0b127b861371b634d17a
Nicole Thurnau <nthurnau@gmail.com>
2016-02-25 17:27:42 -0800

    Mostly fixed edit events in group pages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,9 +27,14 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update_attributes(group_params)
+      redirect_to group_path
+    end
   end
 
   def destroy
